d1b08b0cd3c533aeca469801bd8a9020dadccadc
kulebiaka <mfissyuk20@gmail.com>
2024-04-04 20:26:17 +0200

    fix: app route

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import styles from './App.module.css'
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Home from './pages/Home/Home';
 import CurrentPost from './pages/CurrentPost/CurrentPost';
 
@@ -8,10 +8,11 @@ import CurrentPost from './pages/CurrentPost/CurrentPost';
 const App = () => {
   return (
     <div className={styles.container}>
-      <BrowserRouter basename='/posts'>
+      <BrowserRouter >
         <Routes>
-          <Route index path='/' element={<Home />} />
-          <Route path='/:postId' element={<CurrentPost />} />
+          <Route path='/' element={<Navigate to={'/posts'}/>}/>
+          <Route path='/posts' element={<Home />} />
+          <Route path='/posts/:postId' element={<CurrentPost />} />
         </Routes>
       </BrowserRouter>
     </div>
